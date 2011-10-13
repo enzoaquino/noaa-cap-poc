e4064193f2e9f4c6e5ce391a6c1c1667b402d898
Enzo Aquino <enzo.aquino@gmail.com>
2011-10-13 10:40:32 -0500

    Now transforming message to Java bean, then serializing it to JSON. We will now add it to the RabbitMQ.

---
+++ src/main/java/com/cassidian/cap/Alert.java
@@ -0,0 +1,22 @@
+package com.cassidian.cap;
+
+public class Alert {
+    private String author;
+    private String title;
+    private String summary;
+    private String url;
+
+    public Alert() { }
+
+    public void setAuthor(String author) { this.author = author; }
+    public String getAuthor() { return author; }
+
+    public void setTitle(String title) { this.title = title; }
+    public String getTitle() { return title; }
+
+    public void setSummary(String summary) { this.summary = summary; }
+    public String getSummary()  { return summary; }
+
+    public void setUrl(String url) { this.url = url; }
+    public String getUrl() { return url; }
+}
